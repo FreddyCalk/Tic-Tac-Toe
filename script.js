7bc824fd2fc302d284b04e98e985f2f46eb9b4f9
FreddyCalk <freddycalk@gmail.com>
2015-10-20 13:02:39 -0400

    Tic Tac Toe V2
    Added a win counter, and corrected a bug in the message after the game
    ends.
    Still need to prevent user from making a selection after endGame() is
    called, and then allow selections to be made after the “play again”
    button has been selected

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ var turns = 0;
 var games = 1;
 var player= [];
 var computer = [];
+var winCounter = 0;
+var won = false;
+var message = 'Make a move!';
 var winners = [
 	[1,2,3], [4,5,6],
 	[7,8,9], [1,4,7],
@@ -17,48 +20,96 @@ function placeChar(sqr){
 		document.getElementById('play-square-'+sqr).innerHTML = 'X';
 	// calls the computer to select where to place an O.
 		turns++;
+
 		if(turns<5){
 			AI();
 		}
 		player.push(sqr);
+	}else{
+		alert("You must select an empty square!")
 	}
-	
-	
-	// checkWin()
+	if(!won){
+	checkWin(player,true)	
+	}
 }
 function AI(){
 	// initializes a variable to terminate the loop when the computer makes a move
 	// Try & win, try & block, try a square += 2, or random.
 	var placed = 0;	
+
+	// executes a while loop to generate random numbers until an empty square is found on the board.
 	while(placed!==1){
 		var num = Math.floor(Math.random()*8)+1;
 		if(document.getElementById('play-square-'+num).innerHTML == ''){
 			document.getElementById('play-square-'+num).innerHTML = 'O';
 			placed = 1;
-			computer.push(num)
+			// Adds the square where an "O" was placed to the computer active square array
+			computer.push(num);
+			if(!won){
+			checkWin(computer,false)
+			}
 		}
 	}
-	
+
 }
-function checkWin(){
+function checkWin(sqrs, isPlayer){
 	var thisWin=[]
+	var rowCount = 0;
+	var who = '';
 	for(i=0;i<winners.length;i++){
+		rowCount = 0;
 		thisWin = winners[i];
-
-		for(j=thiswin.length;j>=0;j--){
-
-
+		for(j=thisWin.length-1;j>=0;j--){
+			if(sqrs.indexOf(thisWin[j])>-1){
+				rowCount++;
+			}
 		}
-
+		if((rowCount === 3) && (isPlayer)){
+			won = true;
+			who = 'player';
+		}else if((rowCount === 3)&&(!isPlayer)){
+			won = true;
+			who = 'computer'
+		}
 	}
+		if(won){
+			endGame(who);
+		}
+}
+function endGame(who){
+	
 
+		if(who === 'player'){
+			message = 'You Win! Play again?';
+			document.getElementById('message').innerHTML = message;
+			winCounter++;
+		}else if(who === 'computer'){
+			message = 'You Lose! Play again?';
+			document.getElementById('message').innerHTML = message;
+			winCounter--;
+		}else{
+			message = "It's a draw! Play again?";
+			document.getElementById('message').innerHTML = message;
+		}
+		document.getElementById('button').disabled = false;
+		document.getElementById('win-counter').innerHTML = " "+winCounter;
+	
 }
 
 function newGame(){
 	turns = 0;
-	for(i=0;i<1;i++){
-		// square[i].innerHTML= '';
-		console.dir(square[i])
+	won = false;
+	message = "Make a move!";
+	document.getElementById('message').innerHTML = "Make your move!";
+	for(i=0;i<9;i++){
+		square[i].innerHTML= '';
 	}
 	games++;
+	player = [];
+	computer = [];
+	if(games%2===0){
+		AI()
+	}
+	document.getElementById('button').disabled = true;
+
 }
